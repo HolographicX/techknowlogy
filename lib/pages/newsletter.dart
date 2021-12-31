0ca4130ed5eb1ea9dbe16fc679b4f743d3f644bc
HolographicX <divyamseksaria09@gmail.com>
2021-12-31 17:25:29 +0530

    first commit

---
+++ lib/pages/newsletter.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:techknowlogy/constants.dart';
+
+class Newsletter extends StatefulWidget {
+  const Newsletter({Key? key}) : super(key: key);
+
+  @override
+  _NewsletterState createState() => _NewsletterState();
+}
+
+class _NewsletterState extends State<Newsletter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: primaryColor,
+    );
+  }
+}
